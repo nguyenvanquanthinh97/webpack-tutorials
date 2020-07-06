8d6590d422b9b8a5039cfd6b45eff3bfd30e5236
Thinh97 <nvq.thinh.hcmus.220597@gmail.com>
2020-07-06 22:56:52 +0700

    installing webpack-dev-server to live fixing as create-react-app.

--- webpack.development.config.js
+++ webpack.development.config.js
@@ -1,17 +1,20 @@
 const path = require('path');
-const TerserPlugin = require('terser-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	entry: './src/index.js',
 	output: {
-		filename: 'bundle.[contenthash].js',
+		filename: 'bundle.js',
 		path: path.resolve(__dirname, './dist'),
 		publicPath: ''
 	},
-	mode: 'production',
+	mode: 'development',
+	devServer: {
+		contentBase: path.join(__dirname, 'dist'),
+		index: 'index.html',
+		port: 9000
+	},
 	module: {
 		rules: [
 			{
@@ -20,11 +23,11 @@ module.exports = {
 			},
 			{
 				test: /\.css$/,
-				use: [ MiniCssExtractPlugin.loader, 'css-loader' ]
+				use: [ 'style-loader', 'css-loader' ]
 			},
 			{
 				test: /\.(scss|sass)$/,
-				use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
+				use: [ 'style-loader', 'css-loader', 'sass-loader' ]
 			},
 			{
 				test: /\.js$/,
@@ -44,10 +47,6 @@ module.exports = {
 		]
 	},
 	plugins: [
-		new TerserPlugin(),
-		new MiniCssExtractPlugin({
-			filename: 'styles.[contenthash].css'
-		}),
 		new CleanWebpackPlugin({
 			cleanOnceBeforeBuildPatterns: [ '**/*', path.join(process.cwd(), 'build/**/*') ]
 		}),
---
+++ webpack.production.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		filename: 'bundle.[contenthash].js',
+		path: path.resolve(__dirname, './dist'),
+		publicPath: ''
+	},
+	mode: 'production',
+	module: {
+		rules: [
+			{
+				test: /\.(png|jpg)$/,
+				use: [ 'file-loader' ]
+			},
+			{
+				test: /\.css$/,
+				use: [ MiniCssExtractPlugin.loader, 'css-loader' ]
+			},
+			{
+				test: /\.(scss|sass)$/,
+				use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: [ '@babel/preset-env' ],
+						plugins: [ 'transform-class-properties' ]
+					}
+				}
+			},
+			{
+				test: /\.hbs$/,
+				use: [ 'handlebars-loader' ]
+			}
+		]
+	},
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: 'styles.[contenthash].css'
+		}),
+		new CleanWebpackPlugin({
+			cleanOnceBeforeBuildPatterns: [ '**/*', path.join(process.cwd(), 'build/**/*') ]
+		}),
+		new HtmlWebpackPlugin({
+			title: 'Hello World',
+			description: 'This is a app using webpack manually config',
+			template: 'src/index.hbs',
+			filename: 'index.html'
+		})
+	]
+};
