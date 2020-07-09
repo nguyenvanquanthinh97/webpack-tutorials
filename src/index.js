0bcb8d943469fe152b922b365744c5718961c0c7
Thinh97 <nvq.thinh.hcmus.220597@gmail.com>
2020-07-10 00:19:30 +0700

    Configure eslint but met error in configuring indent

--- src/index.js
+++ src/index.js
@@ -11,6 +11,8 @@ const helloWorldButton = new HelloWorldButton();
 helloWorldButton.render();
 addImage();
 
+console.log('test');
+
 if (process.env.NODE_ENV === 'production') {
 	console.log('Production');
 } else if (process.env.NODE_ENV === 'development') {
